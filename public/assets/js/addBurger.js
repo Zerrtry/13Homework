30cc1370fa9b6a1900bd4760826d2bf874cfb87e
Zerrtry <zerrtry@gmail.com>
2020-07-28 11:46:16 -0500

    all function working on localhost

--- public/assets/js/addBurger.js
+++ public/assets/js/addBurger.js
@@ -4,18 +4,30 @@ $( document ).ready(function() {
     // populate all burgers into html
     const loadAllBurgers = () => {
         
-        $.ajax({ url:"https://eat-ea-burger-hm-13.herokuapp.com/burger-all", method: "GET"}).then((tableData) => {
+        // $.ajax({ url:"https://eat-ea-burger-hm-13.herokuapp.com/burger-all", method: "GET"}).then((tableData) => {
+            $.ajax({ url:"http://localhost:9000/burger-all", method: "GET"}).then((tableData) => {
             console.log(tableData);
             for (let i = 0; i < tableData.length; i++) {
-                const tableList = $("#tableList");
-                const listItem = $("<li class='list-group-item mt-4'>");
-                listItem.append(
-                    $("<br>"),
-                    $(`<h2 class="burgerName">`).text(tableData[i].burger_name),
-                    $("<br>"),
-                    $(`<button type="button" id=${tableData[i].id} class="btn-lg btn-danger deleteBurger">Devour</button>`)
-                );
-                tableList.append(listItem);
+                console.log("tableData.devoured",tableData[i].devoured);
+                if (tableData[i].devoured==0){
+                    const tableList = $("#tableList");
+                    const listItem = $("<li class='list-group-item mt-4'>");
+                    listItem.append(
+                        $("<br>"),
+                        $(`<h2 class="burgerName" id="${tableData[i].burger_name}">`).text(tableData[i].burger_name),
+                        $(`<button type="button" id="${tableData[i].id}" class="btn-lg btn-danger deleteBurger">Devour</button>`)
+                    );
+                    tableList.append(listItem);
+                } else if (tableData[i].devoured==1) {
+                    const tableList2 = $("#tableList-2");
+                    const listItem2 = $("<li class='list-group-item mt-4'>");
+                    listItem2.append(
+                        $("<br>"),
+                        $(`<h2 class="burgerName">`).text(tableData[i].burger_name),
+                        $("<br>")
+                    );
+                    tableList2.append(listItem2);
+                }
             }
         });
     };
@@ -29,27 +41,37 @@ $( document ).ready(function() {
         };
         console.log(newBurger);
 
-        $.ajax({url:"https://eat-ea-burger-hm-13.herokuapp.com/burger-new", method: "POST", data: newBurger}).then (() => {
+        // $.ajax({url:"https://eat-ea-burger-hm-13.herokuapp.com/burger-new", method: "POST", data: newBurger}).then (() => {
+        $.ajax({url:"http://localhost:9000/burger-new", method: "POST", data: newBurger}).then ((tableData) => {
             $("#burger_name").val("");
-            loadAllBurgers();
+            console.log(tableData.insertId);
+            const tableList = $("#tableList");
+            const listItem = $("<li class='list-group-item mt-4'>");
+            listItem.append(
+            $("<br>"),
+            $(`<h2 class="burgerName" id="${newBurger.burger_name}">`).text(newBurger.burger_name),
+            $(`<button type="button" id="${tableData.insertId}" class="btn-lg btn-danger deleteBurger">Devour</button>`)
+            );
+            tableList.append(listItem);
         });
+        
+   
+
     });
 
-    // devoure birger
-    $(document).on('click','.deleteBurger', () => {
+    // devour burger
+    $(document).on('click','.deleteBurger', function() {
         event.preventDefault();
-        const elem1 = $(this).closest('li.list-group-item.mt-4');
-        console.log(elem1);
-        $(this).closest('li.list-group-item.mt-4').remove();
-        const burger_name= $(this).closest(".burgerName").val();
+        const burger_name= $(this).siblings(".burgerName").attr("id");
         console.log(burger_name);
-        const tableList = $("#tableList-2");
-        const listItem = $("<li class='list-group-item mt-4'>");
-        listItem.append(
+        $(this).closest('li.list-group-item.mt-4').remove();
+        const tableList2 = $("#tableList-2");
+        const listItem2 = $("<li class='list-group-item mt-4'>");
+        listItem2.append(
             $("<br>"),
             $("<h2>").text(burger_name),
         );
-        tableList.append(listItem);
+        tableList2.append(listItem2);
         
         const id= $(this).attr("id");
         console.log(id);
@@ -59,7 +81,9 @@ $( document ).ready(function() {
             devoured: 1
         }
 
-        $.ajax({url:"https://eat-ea-burger-hm-13.herokuapp.com/updateBurger", method:"PUT", data:data}).then((data) => {
+        // $.ajax({url:"https://eat-ea-burger-hm-13.herokuapp.com/updateBurger", method:"PUT", data:data}).then((data) => {
+            $.ajax({url:"http://localhost:9000/updateBurger", method:"PUT", data:data}).then((data) => {
+            
             if (data) {
                 alert("Yay! Burger has been devoured!");
             }
