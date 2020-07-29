8a6521738dbdd52444ca5477c53b24180c54fad8
Zerrtry <zerrtry@gmail.com>
2020-07-28 22:14:34 -0500

    start script added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 8080
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
