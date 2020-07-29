64d57217ff1760f01a7890a887cc6e706aa7003d
Zerrtry <zerrtry@gmail.com>
2020-07-29 10:00:34 -0500

    added route to index.html

--- routes.js
+++ routes.js
@@ -4,4 +4,8 @@ exports.route = (app) => {
     app.post("/burger-new", burger.createBurger);
     app.get("/burger-all", burger.getAllBurgers);
     app.put("/updateBurger", burger.updateBurger);
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 }
